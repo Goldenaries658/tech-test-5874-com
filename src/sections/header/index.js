d67b2f6ced0eadf48530a561d4f86a0aeab09f85
Goldenaries658 <59972622+Goldenaries658@users.noreply.github.com>
2020-09-04 16:56:04 +0100

    setup component structure + imported Nav

--- src/sections/header/index.js
+++ src/sections/header/index.js
@@ -1,3 +1,13 @@
+import React from 'react';
+import './css/main.css';
 import logo from './images/digital-spaniel-logo-01.png';
+import Nav from './Nav';
+
+export default function Header() {
+  return (
+    <header>
       <img src={logo} alt="Digital Spaniel Logo" />
       <Nav />
+    </header>
+  );
+}
