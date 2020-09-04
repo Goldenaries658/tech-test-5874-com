48e5a6a1be1878b41d76ece921b21a079fb5ee69
Goldenaries658 <Goldenaries658@users.noreply.github.com>
2020-09-04 11:29:32 +0000

    Prettified Code!

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import React from 'react';
-import './css/main.css';
+import React from "react";
+import "./css/main.css";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
