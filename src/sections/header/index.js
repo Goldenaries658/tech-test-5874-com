49ef9feda8781b9c651f5ea618a6e9e47cbaba6c
Goldenaries658 <59972622+Goldenaries658@users.noreply.github.com>
2020-09-08 13:27:03 +0100

    Header hide on scroll

--- src/sections/header/index.js
+++ src/sections/header/index.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './css/main.css';
 import logo from './images/digital-spaniel-logo-01.png';
 import Nav from './Nav';
 
 export default function Header() {
+  const [scroll, setScroll] = useState({
+    prevPosition: window.pageYOffset,
+    isVisible: true,
+  });
+
+  // Adding event listener on component mount
+  useEffect(() => {
+    const handleScroll = () => {
+      setScroll((scroll) => {
+        const { prevPosition } = scroll;
+        const currentPosition = window.pageYOffset;
+
+        const isVisible = prevPosition > currentPosition;
+
+        return {
+          prevPosition: currentPosition,
+          isVisible,
+        };
+      });
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    // Removing on unmount, for future proofing in case more pages are added
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <header>
+    <header style={scroll.isVisible ? { top: '0px' } : { top: '-120px' }}>
       <img src={logo} alt="Digital Spaniel Logo" />
       <Nav />
     </header>
