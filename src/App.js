8b289d042b7db007d0fedba96bd32e1194c9a49f
Goldenaries658 <59972622+Goldenaries658@users.noreply.github.com>
2020-09-08 21:42:27 +0100

    Defined breakpoints for jsx

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './css/main.css';
 import Header from './sections/Header';
 import SubHeader from './sections/Subheader';
+import Services from './sections/Services';
 
 function App() {
+  const breakpoints = {
+    md: 800,
+  };
+
   return (
     <div className="App">
-      <Header />
+      <Header breakpoints={breakpoints} />
       <SubHeader />
+      <Services />
     </div>
   );
 }
