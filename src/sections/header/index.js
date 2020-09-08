d9dc3332eb502dab37bade306edf9e223b096a0f
Goldenaries658 <59972622+Goldenaries658@users.noreply.github.com>
2020-09-08 21:42:02 +0100

    Responsiveness improvements

--- src/sections/header/index.js
+++ src/sections/header/index.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import './css/header.css';
 import logo from './images/digital-spaniel-logo-01.png';
+import hamburger from './images/hamburger.svg';
 import Nav from './Nav';
+import NavMob from './Hamburger';
 
-export default function Header() {
+export default function Header({ breakpoints }) {
   const [scroll, setScroll] = useState({
     prevPosition: window.pageYOffset,
     isVisible: true,
@@ -33,10 +35,23 @@ export default function Header() {
     };
   }, []);
 
+  const isDesktop = window.innerWidth > breakpoints.md;
+
   return (
-    <header style={scroll.isVisible ? { top: '0px' } : { top: '-120px' }}>
-      <img src={logo} alt="Digital Spaniel Logo" />
-      <Nav />
-    </header>
+    <React.Fragment>
+      <header style={scroll.isVisible ? { top: '0px' } : { top: '-120px' }}>
+        <img id="logo" src={logo} alt="Digital Spaniel Logo" />
+        {isDesktop ? (
+          <Nav />
+        ) : (
+          <img id="hamburger" src={hamburger} alt="Menu icon" />
+        )}
+      </header>
+      {!isDesktop && (
+        <div id="slider">
+          <Nav />
+        </div>
+      )}
+    </React.Fragment>
   );
 }
