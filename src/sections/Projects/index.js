5e122a3856a901a2d6766ec9a6dbdc6fcf31f78c
Goldenaries658 <59972622+Goldenaries658@users.noreply.github.com>
2020-10-04 16:20:31 +0100

    Responsive mosaic layout

--- src/sections/Projects/index.js
+++ src/sections/Projects/index.js
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './css/projects.css';
+import wellies01 from './images/wellies01.png';
+import mackerel from './images/mackerel.png';
+import newspaper from './images/newspaper.png';
+import newspaper02 from './images/newspaper02.png';
+import rider01 from './images/rider01.png';
 
 export default function Projects() {
+  const images = [
+    {
+      id: 0,
+      src: wellies01,
+      alt: 'A pair of stylised wellies',
+      width: 1,
+    },
+    {
+      id: 1,
+      src: newspaper02,
+      alt: 'Newspaper headline',
+      width: 1,
+    },
+    {
+      id: 2,
+      src: mackerel,
+      alt: 'Tesco finest Mackerel Fillets advertisement',
+      width: 1,
+    },
+    {
+      id: 3,
+      src: newspaper,
+      alt: 'Newspaper advertisement',
+      width: 2,
+    },
+    {
+      id: 4,
+      src: rider01,
+      alt: '3d cartoon rendering of boy on a bike',
+      width: 1,
+    },
+  ];
+
   return (
     <section id="projects">
       <article className="info-text">
@@ -27,6 +65,17 @@ export default function Projects() {
           </li>
         </ul>
       </nav>
+
+      <div className="mosaic">
+        {images.map(({ id, src, alt, width }) => (
+          <img
+            className={`mosaic-img width-${width}`}
+            key={id}
+            src={src}
+            alt={alt}
+          />
+        ))}
+      </div>
     </section>
   );
 }
